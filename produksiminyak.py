d5520a308818f81c7b24af9df9259927aa3e9395
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2021-12-17 12:01:31 +0700

    Update produksiminyak.py

--- produksiminyak.py
+++ produksiminyak.py
@@ -82,7 +82,7 @@ y_ = df_[df_['kode_negara']==kode]['produksi'].tolist()
 reg = LinearRegression()
 a=np.array(x_).reshape(-1,1)
 b=np.array(y_)
-if a == [] and b == []:
+if a == [] :
     reg.fit(np.array(x_).reshape(-1,1),np.array(y_))
     m = reg.coef_[0]
     c = reg.intercept_
