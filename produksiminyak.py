74abbdf9a74dc335bca6ee5ccbc433c3663b75b3
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2021-12-17 11:40:22 +0700

    Update produksiminyak.py

--- produksiminyak.py
+++ produksiminyak.py
@@ -80,25 +80,10 @@ x_ = df_[df_['kode_negara']==kode]['tahun'].tolist()
 y_ = df_[df_['kode_negara']==kode]['produksi'].tolist()
 
 reg = LinearRegression()
-try:
-    reg.fit(np.array(x_).reshape(-1,1),np.array(y_))
-    m = reg.coef_[0]
-    c = reg.intercept_
-    y_trend = [m*x+c for x in x_]
-    dic = {'tahun':x_,'produksi':y_}
-    left_col, right_col = st.columns([1,3])
-    left_col.subheader("Tabel produksi minyak mentah ",negara)
-    left_col.dataframe(dic)
-except:
-    st.error('error')
-
+reg.fit(np.array(x_).reshape(-1,1),np.array(y_))
 m = reg.coef_[0]
 c = reg.intercept_
 y_trend = [m*x+c for x in x_]
-if c >= 0:
-    equation = 'y={m:.2f}x+{c:.2f}'.format(m=m,c=c)
-else:
-    equation = 'y={m:.2f}x{c:.2f}'.format(m=m,c=c)
 dic = {'tahun':x_,'produksi':y_}
 dic['trendline'] = y_trend
 fig = px.scatter(pd.DataFrame(dic),x='tahun',y='produksi',trendline='lowess',trendline_options=dict(frac=0.1))
